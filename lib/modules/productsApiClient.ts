e3891d416a6eaad99b92326e579b9db41aebc3a7
Jesús Viafara <jealvito@gmail.com>
2026-06-20 13:05:00 -0500

    Product list and filters page, product detail page completed

--- lib/modules/productsApiClient.ts
+++ lib/modules/productsApiClient.ts
@@ -15,4 +15,18 @@ export const productApi = {
       return { error }
     }
   },
+  productInfo: async (slug: string) => {
+    try {
+      const response = await fetch(`${BASE_URL}/api/${productsEndpoints.info(slug)}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      const res = await response.json()
+      return { res }
+    } catch (error) {
+      console.error(error)
+      return { error }
+    }
+  },
 }
