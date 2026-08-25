cf0f2aa865aad62d9ff099c1f271c07bc7c65ec5
Jesús Viafara <jealvito@gmail.com>
2026-08-24 22:46:09 -0500

    minor style bug fixed, shipping address bug fixed

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,4 +1,5 @@
 import { auth } from '@/lib/auth/auth'
+import Address from '@/lib/models/address'
 import Order from '@/lib/models/order'
 import connectDB from '@/lib/mongodb'
 import responseHandler from '@/lib/responseHandler'
@@ -35,14 +36,20 @@ export async function POST(req: NextRequest) {
     }
 
     const body = await req.json()
+    const address = await Address.findById(body.shippingAddress)
 
+    if (!address) {
+      return responseHandler.badRequest('Address not found')
+    }
     await connectDB()
     const order = await Order.create({
       orderItems: body.orderItems.map((product: IProduct) => ({
         ...product,
         product: product._id,
       })),
-      shippingAddress: body.shippingAddress,
+      shippingAddress: {
+        ...address,
+      },
       paymentId: body.paymentId,
       itemsPrice: body.itemsPrice,
       shippingPrice: body.shippingPrice,
